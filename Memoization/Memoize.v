f44514d0958144fe9b1f9cfd7108d9e67b70f2ff
wkolowski <wkolowski@protonmail.com>
2017-11-22 22:55:43 +0100

    Cleaned up and put together all the various memoization styles for fib. Proved that fibM' = fib.

--- Memoization/Memoize.v
+++ Memoization/Memoize.v
@@ -2,19 +2,17 @@ Add Rec LoadPath "/home/zeimer/Code/Coq".
 
 Require Export RCCBase.
 
+Require Import BTree.
+Require Import BST.
+Require Import TrichDec.
+
 Set Implicit Arguments.
 
-Fixpoint fib (n : nat) : nat :=
-match n with
-    | 0 => 0
-    | 1 => 1
-    | S (S n'' as n') => fib n' + fib n''
-end.
-
-Lemma fib_SS :
-  forall n : nat, fib (S (S n)) = fib n + fib (S n).
+Lemma prod_eq :
+  forall (A B : Type) (a a' : A) (b b' : B),
+    (a, b) = (a', b') -> a = a' /\ b = b'.
 Proof.
-  intros. cbn. omega.
+  intros. inv H.
 Qed.
 
 Fixpoint fromTo (a b : nat) : list nat :=
@@ -24,19 +22,7 @@ match b with
     | S b' => fromTo a b' ++ [b]
 end.
 
-Definition test := [0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 10].
-
-Time Compute map fib (fromTo 0 24).
-
-Time Compute fib 24.
-
-Fixpoint find {A : Type} (n : nat) (l : list (nat * A)) : option A :=
-match l with
-    | [] => None
-    | (m, v) :: t => if n =? m then Some v else find n t
-end.
-
-Axiom wut : False.
+Axiom wut : False. (* TODO *)
 
 Instance KVP (A : LinDec) (B : Type) : LinDec :=
 {
@@ -51,90 +37,108 @@ Proof.
   end; cbn in *; dec.
   cut False.
     inversion 1.
-    apply wut.
+    apply wut. (* TODO *)
 Defined.
 
-Function aux (n : nat) (acc : list (nat * nat)) : nat :=
+Function find {A : TrichDec} {B : Type} (k : A) (t : BTree (KVP A B))
+  : option B :=
+match t with
+    | empty => None
+    | node p l r =>
+        match k <?> fst p with
+            | Lt => find k l
+            | Eq => Some (snd p)
+            | Gt => find k r
+        end
+end.
+
+Definition FibAcc := KVP natlt natlt.
+
+Function fib (n : nat) : nat :=
+match n with
+    | 0 => 0
+    | 1 => 1
+    | S (S n'' as n') => fib n' + fib n''
+end.
+
+Lemma fib_SS :
+  forall n : nat, fib (S (S n)) = fib n + fib (S n).
+Proof.
+  intros. cbn. omega.
+Qed.
+
+Fixpoint fib_tr_aux (n a b : nat) : nat :=
+match n with
+    | 0 => a
+    | S n' => fib_tr_aux n' b (a + b)
+end.
+
+Definition fib_tr (n : nat) := fib_tr_aux n 0 1.
+
+Lemma fib_tr_aux_spec :
+  forall n k : nat,
+    fib_tr_aux (1 + k) (fib n) (fib (1 + n)) = fib (1 + k + n).
+Proof.
+  intros. generalize dependent n.
+  induction k; intros.
+    cbn. trivial.
+    change (fib_tr_aux (S k) (fib (1 + n)) (fib n + fib (1 + n)) =
+            fib (1 + S k + n)).
+      replace (fib n + fib (1 + n)) with (fib (2 + n)).
+        rewrite IHk. f_equal. omega.
+        cbn. omega.
+Qed.
+
+Theorem fib_tr_is_fib : fib_tr = fib.
+Proof.
+  ext n. unfold fib_tr. destruct n.
+    cbn. trivial.
+    change 1 with (fib 1). change 0 with (fib 0).
+      rewrite fib_tr_aux_spec. f_equal. cbn. omega.
+Qed.
+
+Let list_map := @map.
+
+Function fibM_aux (n : nat) (acc : BTree FibAcc) : nat :=
 match n with
     | 0 => 0
     | 1 => 1
     | S (S n'' as n') =>
         match find n'' acc with
             | None =>
-                let a := aux n'' acc in
-                let b := aux n' ((n'', a) :: acc) in a + b
+                let a := fibM_aux n'' acc in
+                let b := fibM_aux n' (@BTree_ins FibAcc (n'', a) acc) in a + b
             | Some a =>
                 match find n' acc with
                     | None =>
-                        let b := aux n' acc in a + b
+                        let b := fibM_aux n' acc in a + b
                     | Some b => a + b
                 end
         end
 end.
 
-Definition fibM n := aux n [(0, 0); (1, 1)].
-
-Compute map fibM test.
-Time Compute fibM 24.
-
-Let list_map := map.
-
-Require Import BTree.
-Require Import BST.
-Require Import TrichDec.
-
-Fixpoint find' {A : TrichDec} {B : Type} (k : A) (t : BTree (KVP A B))
-  : option B :=
-match t with
-    | empty => None
-    | node p l r =>
-        match k <?> fst p with
-            | Lt => find' k l
-            | Eq => Some (snd p)
-            | Gt => find' k r
-        end
-end.
-
-Definition FibAcc := KVP natlt natlt.
-
-Function aux' (n : nat) (acc : BTree FibAcc) : nat :=
-match n with
-    | 0 => 0
-    | 1 => 1
-    | S (S n'' as n') =>
-        match find' n'' acc with
-            | None =>
-                let a := aux' n'' acc in
-                let b := aux' n' (@BTree_ins FibAcc (n'', a) acc) in a + b
-            | Some a =>
-                match find' n' acc with
-                    | None =>
-                        let b := aux' n' acc in a + b
-                    | Some b => a + b
-                end
-        end
-end.
-
-Definition fibM' n :=
-  aux' n (@fromList FibAcc [(0, 0); (1, 1)]).
-
-Time Compute list_map fibM' (fromTo 0 24).
+Definition fibM (n : nat) : nat :=
+  fibM_aux n (@fromList FibAcc [(0, 0); (1, 1)]).
 
 Definition acc_correct {A : TrichDec} {B : Type}
   (f : A -> B) (acc : BTree (KVP A B)) : Prop :=
     forall (k : A) (v : B), elem (k, v) acc -> v = f k.
 
-Lemma find'_reflect :
+Lemma find_elem :
   forall (A : TrichDec) (B : Type) (k : A) (v : B) (t : BTree (KVP A B)),
-    find' k t = Some v <-> elem (k, v) t.
+    find k t = Some v -> elem (k, v) t.
 Proof.
-Admitted.
+  intros. functional induction @find A B k t; intros; try congruence.
+    eauto.
+    destruct p; cbn in *. trich. inv H.
+    eauto.
+Qed.
 
 Ltac fib := 
 repeat match goal with
     | p : ?P, H : ?P -> _ |- _ => destruct (H p); clear H
-    | H : aux' _ _ = fib _ |- _ => rewrite H in *; clear H
-    | H : find' _ _ = Some _ |- _ => rewrite find'_reflect in H
+    | H : _ = fib _ |- _ => rewrite H in *; clear H
+    | H : find _ _ = Some _ |- _ => apply find_elem in H
     | H : acc_correct _ _ |- _ => red in H
 end;
   try (split; try red; intros);
@@ -149,131 +153,114 @@ end; cbn; auto; try omega.
 Theorem aux'_correct :
   forall (n : nat) (acc : BTree FibAcc),
     acc_correct fib acc ->
-      acc_correct fib (@BTree_ins FibAcc (n, aux' n acc) acc) /\
-      aux' n acc = fib n.
+      acc_correct fib (@BTree_ins FibAcc (n, fibM_aux n acc) acc) /\
+      fibM_aux n acc = fib n.
 Proof.
   intros n acc.
-  functional induction aux' n acc; intros; fib.
+  functional induction fibM_aux n acc; intros; fib.
 Qed.
 
-Theorem fibM'_correct :
-  forall n : nat, fibM' n = fib n.
+Theorem fibM_correct : fibM = fib.
 Proof.
-  intros. unfold fibM'. apply aux'_correct.
+  ext n. unfold fibM. apply aux'_correct.
   red. intros. inv H; inv H1; inv H0.
 Qed.
 
-Theorem acc_correct'_ins :
+Theorem acc_correct_ins_fibM_aux :
   forall (n : nat) (acc : BTree FibAcc),
-    acc_correct fib acc -> aux' n acc = fib n ->
-      acc_correct fib (@BTree_ins FibAcc (n, aux' n acc) acc).
+    acc_correct fib acc -> fibM_aux n acc = fib n ->
+      acc_correct fib (@BTree_ins FibAcc (n, fibM_aux n acc) acc).
 Proof.
   intros. red; intros.
   destruct (@elem_ins FibAcc _ _ _ H1); fib.
 Qed.
 
-Theorem aux'_correct' :
+Theorem fibM_aux_correct :
   forall (n : nat) (acc : BTree FibAcc),
-    acc_correct fib acc -> aux' n acc = fib n.
+    acc_correct fib acc -> fibM_aux n acc = fib n.
 Proof.
-  intros. functional induction aux' n acc.
+  intros. functional induction fibM_aux n acc.
     fib.
     fib.
     erewrite IHn1, IHn0; eauto; cbn.
       omega.
-      apply acc_correct'_ins; auto.
-    rewrite find'_reflect in e0. rewrite (H _ _ e0).
+      apply acc_correct_ins_fibM_aux; auto.
+    apply find_elem in e0. rewrite (H _ _ e0).
       erewrite IHn0; eauto; cbn. omega.
     fib.
 Qed.
 
-Theorem fibM'_correct' :
-  forall n : nat, fibM' n = fib n.
+Theorem fibM_correct' : fibM = fib.
 Proof.
-  intros. unfold fibM'. apply aux'_correct'.
+  ext n. unfold fibM. apply fibM_aux_correct.
   red. intros. inv H; inv H1; inv H0.
 Qed.
 
 Definition bind {A : TrichDec} {B : Type} (a : A)
   (f : A -> BTree (KVP A B) -> B) (acc : BTree (KVP A B))
   : B * BTree (KVP A B) :=
-match find' a acc with
+match find a acc with
     | None => let b := f a acc in (b, @BTree_ins (KVP A B) (a, b) acc)
     | Some b => (b, acc)
 end.
 
-Function aux2 (n : nat) (acc : BTree FibAcc) : nat :=
+Function fibM'_aux (n : nat) (acc : BTree FibAcc) : nat :=
 match n with
     | 0 => 0
     | 1 => 1
     | S (S n'' as n') =>
-        let '(a, acc') := bind n'' aux2 acc in
-        let '(b, acc'') := bind n' aux2 acc' in a + b
+        let '(a, acc') := bind n'' fibM'_aux acc in
+        let '(b, acc'') := bind n' fibM'_aux acc' in a + b
 end.
 
-Definition fibM2 n := aux2 n (@fromList FibAcc [(0, 0); (1, 1)]).
+Definition fibM' n := fibM'_aux n (@fromList FibAcc [(0, 0); (1, 1)]).
 
-Time Compute list_map fib (fromTo 0 24).
-Time Compute list_map fibM2 (fromTo 0 24).
-
-Lemma bind_correct :
+Lemma bind_correct_fibM'_aux :
   forall (k : nat) (acc : BTree FibAcc),
-    acc_correct fib acc -> aux2 k acc = fib k ->
-      fst (bind k aux2 acc) = fib k.
+    acc_correct fib acc -> fibM'_aux k acc = fib k ->
+      fst (@bind natlt nat k fibM'_aux acc) = fib k.
 Proof.
-  intros. unfold bind. case_eq (find' k acc); intros; cbn.
-    apply find'_reflect in H1. red in H. rewrite (H _ _ H1). trivial.
+  intros. unfold bind. case_eq (@find natlt nat k acc); intros; cbn.
+    apply find_elem in H1. red in H. rewrite (H _ _ H1). trivial.
     assumption.
 Qed.
 
-Lemma prod_eq :
-  forall (A B : Type) (a a' : A) (b b' : B),
-    (a, b) = (a', b') -> a = a' /\ b = b'.
+Theorem acc_correct_ins_fibM'_aux :
+  forall (n : nat) (acc : BTree FibAcc),
+    acc_correct fib acc -> fibM'_aux n acc = fib n ->
+      acc_correct fib (@BTree_ins FibAcc (n, fibM'_aux n acc) acc).
 Proof.
-  intros. inv H.
+  intros. red; intros.
+  destruct (@elem_ins FibAcc _ _ _ H1); fib.
 Qed.
 
-Theorem aux2_correct :
+Theorem fibM'_aux_correct :
   forall (n : nat) (acc : BTree FibAcc),
-    acc_correct fib acc ->
-      acc_correct fib (@BTree_ins FibAcc (n, aux2 n acc) acc) /\
-      aux2 n acc = fib n. (* /\
-      bind n aux2 acc = (fib n, @BTree_ins FibAcc (n, fib n) acc).*)
+    acc_correct fib acc -> fibM'_aux n acc = fib n.
 Proof.
-  intros n acc. functional induction aux2 n acc; intros.
-    fib. (* unfold bind. cbn. remember 0 as n. destruct (@find' natlt nat n acc).
-      split; auto.*)
-    fib.
-    assert (fst (bind n'' aux2 acc) = a) by (rewrite e0; auto).
-    assert (fst (bind (S n'') aux2 acc') = b) by (rewrite e1; auto).
-      rewrite bind_correct in *; auto.
-        rewrite <- ?H0, <- ?H1 in *. split; try red; intros; fib.
-        red in H.
-(*
-    apply prod_eq in e0; apply prod_eq in e1. *.
-    case_eq (bind n'' aux2 acc); intros. Check bind_correct. pose (bind_correct e0).
-    case_eq (find' n'' acc); case_eq (find' (S n'') acc');
-    unfold bind in *; intros; rewrite ?H0, ?H1, ?find'_reflect in *.
-      inv e0; inv e1. rewrite (H _ _ H1), (H _ _ H0). split.
-        red. intros. fib.
-        fib.
-      inv e0. destruct (prod_eq e1).
-        rewrite (H _ _ H1), <- ?H2, <- ?H3 in *. split.
-          red. intros. fib.*)
-Restart.
-  fix 1. destruct n as [| [| n']]; intros.
-    fib.
-    fib.
-    cbn; unfold bind.
-    case_eq (@find' natlt nat n' acc). case_eq (@find' natlt nat (S n') acc);
-    intros; rewrite ?H0, ?H1, ?find'_reflect in *.
-      rewrite (H _ _ H1), (H _ _ H0). split.
-        red. intros. fib.
-        fib.
-      destruct (aux2_correct (S n') _ H). rewrite (H _ _ H1), H3. fib.
-        
-        edestruct (aux2_correct (S (S n'))). eauto.
-Abort.
+  intro. functional induction fib n; cbn; intros.
+    trivial.
+    trivial.
+    destruct (@bind natlt nat n'' fibM'_aux acc) as [a acc'] eqn: H1;
+    destruct (@bind natlt nat (S n'') fibM'_aux acc') as [b acc''] eqn: H2.
+    unfold bind in *.
+    case_eq (@find natlt nat n'' acc);
+    case_eq (@find natlt nat (S n'') acc'); intros;
+    rewrite H0, H3 in *;
+    repeat match goal with
+        | H : find _ _ = Some _ |- _ => apply find_elem in H
+    end.
+      inv H1; inv H2. rewrite (H _ _ H0), (H _ _ H3). cbn; omega.
+      inv H1. destruct (prod_eq H2).
+        rewrite (H _ _ H3), <- H1, IHn0. cbn; omega. assumption.
+      destruct (prod_eq H1). inv H2. destruct (@elem_ins FibAcc _ _ _ H0).
+        inv H2. omega.
+        rewrite (H _ _ H2), IHn1. cbn; omega. assumption.
+      destruct (prod_eq H1), (prod_eq H2); subst. rewrite IHn0, IHn1.
+        cbn; omega.
+        assumption.
+        apply acc_correct_ins_fibM'_aux; auto.
+Qed.
 
 Fixpoint wutzor (n : nat) : nat :=
 match n with
