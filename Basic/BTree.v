f6ec2fbacd65245789b76ad75293fa32d7d1f2b5
wkolowski <wkolowski@protonmail.com>
2018-03-31 00:24:18 +0200

    Started a file with Church enconding for lists.

--- Basic/BTree.v
+++ Basic/BTree.v
@@ -63,7 +63,8 @@ match t with
     | node v l r => BTree_toList l ++ v :: BTree_toList r
 end.
 
-Function BTree_toList'_aux {A : LinDec} (t : BTree A) (acc : list A) : list A :=
+Function BTree_toList'_aux
+  {A : LinDec} (t : BTree A) (acc : list A) : list A :=
 match t with
     | empty => acc
     | node v l r => BTree_toList'_aux l (v :: BTree_toList'_aux r acc)
@@ -101,7 +102,7 @@ end; auto.
 
 (** * Theorems *)
 
-(** Propertes of [elem] and [elem_decb]. *)
+(** Properties of [elem] and [elem_decb]. *)
 Lemma elem_decb_reflect :
   forall (A : LinDec) (x : A) (t : BTree A),
     reflect (elem x t) (elem_decb x t).
@@ -267,4 +268,203 @@ Lemma isEmpty_count_BT :
 Proof.
   split; destruct t; cbn; try congruence.
     intro. specialize (H c). dec.
+Qed.
+
+(** [mirror] *)
+
+Fixpoint mirror {A : Type} (t : BTree A) : BTree A :=
+match t with
+    | empty => empty
+    | node v l r => node v (mirror r) (mirror l)
+end.
+
+Lemma mirror_inv :
+  forall (A : Type) (t : BTree A),
+    mirror (mirror t) = t.
+Proof.
+  induction t; cbn; rewrite ?IHt1, ?IHt2; reflexivity.
+Qed.
+
+Lemma elem_mirror :
+  forall (A : Type) (x : A) (t : BTree A),
+    elem x (mirror t) <-> elem x t.
+Proof.
+  assert (forall (A : Type) (x : A) (t : BTree A),
+            elem x t -> elem x (mirror t)).
+    induction 1; cbn; auto.
+    split; intro.
+      rewrite <- mirror_inv. apply H, H0.
+      apply H, H0.
+Qed.
+
+Lemma size_mirror :
+  forall (A : Type) (t : BTree A),
+    size (mirror t) = size t.
+Proof.
+  induction t; cbn; rewrite ?IHt1, ?IHt2, 1?plus_comm; reflexivity.
+Qed.
+
+Lemma mirror_singleton :
+  forall (A : Type) (x : A),
+    mirror (singleton x) = singleton x.
+Proof. reflexivity. Qed.
+
+(** [replicate] *)
+
+Fixpoint replicate {A : Type} (n : nat) (x : A) : BTree A :=
+match n with
+    | 0 => empty
+    | S n' => node x (replicate n' x) (replicate n' x)
+end.
+
+Function pow2 (n : nat) : nat :=
+match n with
+    | 0 => 1
+    | S n' => 2 * pow2 n'
+end.
+
+Lemma pow2_1plus :
+  forall (A : Type) (n : nat),
+    exists m : nat, pow2 n = 1 + m.
+Proof.
+  induction n as [| n']; cbn.
+    exists 0. reflexivity.
+    destruct IHn' as [m IH]. rewrite !IH. exists (1 + 2 * m). cbn.
+      rewrite <- plus_n_Sm, plus_0_r. reflexivity.
+Qed.
+
+Lemma size_replicate :
+  forall (A : Type) (n : nat) (x : A),
+    size (replicate n x) = pow2 n - 1.
+Proof.
+  induction n as [| n']; cbn; intros.
+    reflexivity.
+    rewrite !IHn', plus_0_r. destruct (pow2_1plus A n').
+      rewrite !H. cbn. rewrite <- !minus_n_O, plus_n_Sm. reflexivity.
+Qed.
+
+(** [takeWhile] *)
+
+Fixpoint takeWhile {A : Type} (p : A -> bool) (t : BTree A) : BTree A :=
+match t with
+    | empty => empty
+    | node v l r =>
+        if p v
+        then node v (takeWhile p l) (takeWhile p r)
+        else empty
+end.
+
+Lemma size_takeWhile :
+  forall (A : Type) (p : A -> bool) (t : BTree A),
+    size (takeWhile p t) <= size t.
+Proof.
+  induction t; cbn.
+    apply le_0_n.
+    destruct (p a); cbn.
+      apply le_n_S. omega.
+      apply le_0_n.
+Qed.
+
+Fixpoint inorder {A : Type} (t : BTree A) : list A :=
+match t with
+    | empty => []
+    | node v l r => inorder l ++ v :: inorder r
+end.
+
+Fixpoint preorder {A : Type} (t : BTree A) : list A :=
+match t with
+    | empty => []
+    | node v l r => v :: (preorder l ++ preorder r)
+end.
+
+Fixpoint postorder {A : Type} (t : BTree A) : list A :=
+match t with
+    | empty => []
+    | node v l r => postorder l ++ postorder r ++ [v]
+end.
+
+Definition leaf {A : Type} (x : A) : BTree A :=
+  node x empty empty.
+
+Let t :=
+  node 1
+    (node 2 (leaf 4) (leaf 5))
+    (leaf 3).
+
+Compute inorder t.
+Compute preorder t.
+Compute postorder t.
+
+Fixpoint wut {A : Type} (t : BTree A) : nat :=
+match t with
+    | empty => 1
+    | node v l r => 2 + wut l + wut r
+end.
+
+Fixpoint sumOfWuts {A : Type} (l : list (BTree A)) : nat :=
+match l with
+    | [] => 0
+    | t :: ts => 1 + wut t + sumOfWuts ts
+end.
+
+Lemma sumOfWuts_app :
+  forall (A : Type) (l1 l2 : list (BTree A)),
+    sumOfWuts (l1 ++ l2) = sumOfWuts l1 + sumOfWuts l2.
+Proof.
+  induction l1 as [| h t]; cbn; intros.
+    reflexivity.
+    rewrite IHt. rewrite plus_assoc. reflexivity.
+Qed.
+
+Function bfs_aux
+  {A : Type} (ts : list (BTree A)) (acc : list A) {measure sumOfWuts ts}
+  : list A :=
+match ts with
+    | [] => acc
+    | empty :: ts' => bfs_aux ts' acc
+    | node v l r :: ts' =>
+        bfs_aux (ts' ++ [l; r]) (v :: acc)
+end.
+Proof.
+  intros. subst. cbn. apply le_S, le_n.
+  intros. subst. cbn. rewrite sumOfWuts_app. cbn. omega.
+Defined.
+
+Definition bfs {A : Type} (t : BTree A) : list A :=
+  rev (bfs_aux A [t] []).
+
+Compute bfs t.
+
+Fixpoint sumOfSizes {A : Type} (l : list (BTree A)) : nat :=
+match l with
+    | [] => 0
+    | h :: t => size h + sumOfSizes t
+end.
+
+Lemma sumOfSizes_app :
+  forall (A : Type) (l1 l2 : list (BTree A)),
+    sumOfSizes (l1 ++ l2) = sumOfSizes l1 + sumOfSizes l2.
+Proof.
+  induction l1 as [| h1 t1]; cbn; intros.
+    reflexivity.
+    rewrite IHt1. apply plus_assoc.
+Qed.
+
+Lemma length_bfs_aux :
+  forall (A : Type) (l : list (BTree A)) (acc : list A),
+    length (bfs_aux A l acc) = sumOfSizes l + length acc.
+Proof.
+  intros. functional induction bfs_aux A l acc; cbn.
+    reflexivity.
+    apply IHl0.
+    rewrite IHl0, sumOfSizes_app. cbn. omega.
+Qed.
+
+Lemma length_bfs :
+  forall (A : Type) (t : BTree A),
+    length (bfs t) = size t.
+Proof.
+  intros. unfold bfs.
+  rewrite rev_length, length_bfs_aux.
+  cbn. rewrite ?plus_0_r. reflexivity.
 Qed.
