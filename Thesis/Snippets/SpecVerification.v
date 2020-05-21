ebf61a5ce1e3ea6f28747ccd8f0147ac3215f24d
wkolowski <wkolowski@protonmail.com>
2020-05-21 23:36:14 +0200

    Thesis, section 1.6: started describing the example Coq code listing.

--- Thesis/Snippets/SpecVerification.v
+++ Thesis/Snippets/SpecVerification.v
@@ -1,3 +1,6 @@
+Require Import List Lia.
+Import ListNotations.
+
 Print list.
 (*
 Inductive list (A : Type) : Type :=
@@ -5,41 +8,31 @@ Inductive list (A : Type) : Type :=
     | cons : A -> list A -> list A
 *)
 
-Require Import List.
-Import ListNotations.
-
 Print app.
 (*
-fun A : Type =>
-fix app (l m : list A) {struct l} : list A :=
-  match l with
-  | [] => m
-  | a :: l1 => a :: app l1 m
-  end
-    : forall A : Type, list A -> list A -> list A
+fix app {A : Type} (l1 l2 : list A) {struct l1} : list A :=
+match l1 with
+  | [] => l2
+  | h :: t => h :: app t l2
+end
+  : forall A : Type, list A -> list A -> list A
 *)
 
 Record rev_spec {A : Type} (f : list A -> list A) : Prop :=
 {
-    f_app_comm :
-      forall l1 l2 : list A, f (l1 ++ l2) = f l2 ++ f l1;
-    f_singl :
-      forall x : A, f [x] = [x];
+    f_app   : forall l1 l2 : list A, f (l1 ++ l2) = f l2 ++ f l1;
+    f_singl : forall x : A, f [x] = [x];
 }.
-
-Require Import Lia.
 
 Lemma rev_unique :
   forall {A : Type} (f g : list A -> list A),
-    rev_spec f -> rev_spec g ->
-      forall l : list A, f l = g l.
+    rev_spec f -> rev_spec g -> forall l : list A, f l = g l.
 Proof.
   intros A f g [Hfc Hfs] [Hgc Hgs].
   induction l as [| h t]; cbn.
-    specialize (Hfc [] []); specialize (Hgc [] []); cbn in *.
-      apply (f_equal (@length _)) in Hfc;
-      apply (f_equal (@length _)) in Hgc.
-      rewrite app_length in *.
+    assert (Hfc' := f_equal (@length _) (Hfc [] []));
+    assert (Hgc' := f_equal (@length _) (Hgc [] [])).
+      cbn in *. rewrite app_length in *.
       destruct (f []), (g []).
         reflexivity.
         1-3: cbn in *; lia.
