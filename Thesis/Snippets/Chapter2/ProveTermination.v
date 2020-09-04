7775d3f6367015f8f445d73fc65de0b4a0226bf5
wkolowski <wkolowski@protonmail.com>
2020-09-04 23:35:39 +0200

    Some coding, some planning, some code listings added to the text.

--- Thesis/Snippets/Chapter2/ProveTermination.v
+++ Thesis/Snippets/Chapter2/ProveTermination.v
@@ -47,10 +47,11 @@ Inductive QSDom (A : QSArgs) : list A -> Type :=
 Fixpoint qs' {A : QSArgs} {l : list A} (d : QSDom A l) : list A :=
 match d with
     | Short _ _ _ => adhoc l
-    | Long _ _ pivot _ eq _ ltd gtd => qs' ltd ++ pivot :: eq ++ qs' gtd
+    | Long _ _ pivot _ eq _ ltd gtd =>
+        qs' ltd ++ pivot :: eq ++ qs' gtd
 end.
 
-Definition QSDom_all :
+Lemma QSDom_all :
   forall (A : QSArgs) (l : list A), QSDom A l.
 Proof.
   intro A.
@@ -61,10 +62,29 @@ Proof.
       destruct (choosePivot h t) as [pivot rest] eqn: Hpivot;
       destruct (partition pivot rest) as [[lt eq] gt] eqn: Hpartition.
       econstructor 2; try eassumption.
-        apply IH. red. eapply le_lt_trans.
+        apply IH; red. apply le_lt_trans with (length rest).
+          admit.
+          admit.
+        apply IH; red. apply le_lt_trans with (length rest).
+          admit.
+          admit.
+Abort.
+
+Lemma QSDom_all :
+  forall (A : QSArgs) (l : list A), QSDom A l.
+Proof.
+  intro A.
+  apply well_founded_induction_type with (R := ltof _ (@length A)).
+    apply well_founded_ltof.
+    intros l IH. destruct (short l) as [[h t] |] eqn: Hshort.
+      Focus 2. constructor. assumption.
+      destruct (choosePivot h t) as [pivot rest] eqn: Hpivot;
+      destruct (partition pivot rest) as [[lt eq] gt] eqn: Hpartition.
+      econstructor 2; try eassumption.
+        apply IH; red. apply le_lt_trans with (length rest).
           apply (partition_len_lt Hpartition).
           rewrite (choosePivot_len Hpivot). apply (short_len Hshort).
-        apply IH. red. eapply le_lt_trans.
+        apply IH; red. apply le_lt_trans with (length rest).
           apply (partition_len_gt Hpartition).
           rewrite (choosePivot_len Hpivot). apply (short_len Hshort).
 Defined.
@@ -107,3 +127,22 @@ Proof.
 Defined.
 
 Compute qs QSArgs_nat [4; 3; 2; 1].
+
+Require Import Recdef.
+
+Function qsf (A : QSArgs) (l : list A) {measure length l} : list A :=
+match short l with
+    | None => adhoc l
+    | Some (h, t) =>
+        let '(pivot, rest) := choosePivot h t in
+        let '(lt, eq, gt)  := partition pivot rest in
+          qsf A lt ++ pivot :: eq ++ qsf A gt
+end.
+Proof.
+  intros; subst. apply le_lt_trans with (length rest).
+    apply (partition_len_gt teq2).
+    rewrite (choosePivot_len teq1). apply (short_len teq).
+  intros; subst. apply le_lt_trans with (length rest).
+    apply (partition_len_lt teq2).
+    rewrite (choosePivot_len teq1). apply (short_len teq).
+Defined.
